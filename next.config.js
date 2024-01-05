05cf3fe5edc09103457fb916ad08b9fb88d0cdec
shaiMatz <shyshir@gmail.com>
2024-01-05 14:59:13 +0200

    first commit 	Setting Up the Environment

--- next.config.js
+++ next.config.js
@@ -3,4 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+module.exports = {
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+
+  serverRuntimeConfig: {
+    apiKey: process.env.API_KEY,
+  },
+  publicRuntimeConfig: {
+    apiKey: process.env.API_KEY,
+  },
+
+ 
+
+}
 module.exports = nextConfig
